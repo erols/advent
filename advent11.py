855694c214aa9fadad629c44776f850c43b05fb5
esuleyman <erol.suleyman@gmail.com>
2020-12-11 18:22:51 +0100

    day 10

--- advent11.py
+++ advent11.py
@@ -7,21 +7,22 @@ def set_seat_state(c, sl):
     unoccupied = 0
     if sl[c[0]][c[1]] == '.':
         return
-    for i in range(3):
-        for j in range(3):
+    for i in range(-1, 1):
+        for j in range(-1, 1):
             if i == 1 and j == 1:
                 continue
-            if c[0] - i < 0:
+            if c[0] + i < 0:
                 continue
             if c[0] + i == len(sl[0]):
                 continue
-            if c[1] - j < 0:
+            if c[1] + j < 0:
                 continue
             if c[1] + j == len(sl):
                 continue
-            if sl[i][j] == '#':
+            if sl[c[0]+i][c[1]+j] == '#':
                 occupied = occupied + 1
-            elif sl[i][j] == 'L':
+            # elif sl[c[0]+i][c[1]+j] == 'L' or sl[c[0]+i][c[1]+j] == '.':
+            elif sl[c[0] + i][c[1] + j] == 'L':
                 unoccupied = unoccupied + 1
         if sl[c[0]][c[1]] == 'L' and occupied == 0:
             sl[c[0]][c[1]] = '#'
@@ -44,13 +45,23 @@ def count_seats(sl):
     return occupied, unoccupied, blank
 
 
+def copy_seats(sl):
+    copy_of_seats = []
+    for r in sl:
+        copy = [x for x in r]
+        copy_of_seats.append(copy)
+    return copy_of_seats
+
+
 def part_a(sl):
-    previous_seats = sl.copy()
+    previous_seats = copy_seats(sl)
     while True:
         for i in range(len(sl)):
             for j in range(len(sl[0])):
                 set_seat_state([i, j], sl)
         if sl == previous_seats:
+            for r in sl:
+                print(r)
             return count_seats(sl)
         else:
             previous_seats = sl.copy()
