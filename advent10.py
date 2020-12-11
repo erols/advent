08a4e07f6dff41b11b7fb5cd98ade5009b4031c4
erols <erol.suleyman@gmail.com>
2020-12-11 09:32:08 +0100

    day 11 start

--- advent10.py
+++ advent10.py
@@ -7,7 +7,7 @@ adapters.insert(0, 0)
 
 def part_a():
     jolts = [0, 0, 0]
-    for i, v in enumerate(adapters):
+    for i in range(len(adapters)):
         if i == len(adapters) - 1:
             break
         diff = adapters[i+1] - adapters[i]
@@ -21,8 +21,17 @@ def part_a():
     return jolts[0]*jolts[2]
 
 
-def part_b():
-    pass
+with open('input10') as f:
+    numbers = [0] + sorted([int(x) for x in f.readlines()])
+
+placeholders = [1] + [0 for x in range(len(numbers)-1)]
+for x in range(len(numbers)):
+    for y in range(1, 4):
+        if numbers[x] + y in numbers:
+            placeholders[numbers.index(numbers[x] + y)] += placeholders[x]
+            print(placeholders)
+
+print(placeholders[-1])
 
 
-print(part_a())
+# print(part_a())
